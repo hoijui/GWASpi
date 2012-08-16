0af061f34c81afcf532d5c3a2729666df30045b2
hoijui <hoijui.quaero@gmail.com>
2012-08-16 20:51:54 +0200

    remove template headers

--- src/main/java/org/gwaspi/threadbox/SwingWorkerItemList.java
+++ src/main/java/org/gwaspi/threadbox/SwingWorkerItemList.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.gwaspi.threadbox;
 
 import java.util.ArrayList;
--- src/test/java/org/gwaspi/gui/qqPlot.java
+++ src/test/java/org/gwaspi/gui/qqPlot.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* ===========================================================
  * JFreeChart : a free chart library for the Java(tm) platform
