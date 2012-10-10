353dbf5c18f92b088ba6a382b3cfbd9cb6f5f3da
hoijui <hoijui.quaero@gmail.com>
2012-10-10 10:35:54 +0200

    cleanup MatrixMetadata
    * remove lots of duplicate code
    * reduce String concatenation

--- src/main/java/org/gwaspi/netCDF/matrices/MatrixMetadata.java
+++ src/main/java/org/gwaspi/netCDF/matrices/MatrixMetadata.java
@@ -31,6 +31,10 @@ public class MatrixMetadata {
 
 	private final Logger log = LoggerFactory.getLogger(MatrixManager.class);
 
+	private static final String SQL_STATEMENT_SELECT_MATRIX_BY_ID
+			= "SELECT * FROM " + cDBGWASpi.SCH_MATRICES + "." + cDBMatrix.T_MATRICES + " WHERE " + cDBMatrix.f_ID + "=%d  WITH RR";
+	private static final String SQL_STATEMENT_SELECT_MATRIX_BY_NAME
+			= "SELECT * FROM " + cDBGWASpi.SCH_MATRICES + "." + cDBMatrix.T_MATRICES + " WHERE " + cDBMatrix.f_NETCDF_NAME + "='%s'  WITH RR";
 	private int matrixId = Integer.MIN_VALUE;
 	private String matrixFriendlyName = "";
 	private String matrixNetCDFName = "";
@@ -50,25 +54,68 @@ public class MatrixMetadata {
 	private String input_location = ""; // input_location VARCHAR(1000)
 	private String loaded = ""; // loaded CHAR(1)
 
-	public MatrixMetadata(int _matrixId) throws IOException {
+	public MatrixMetadata(int matrixId) throws IOException {
+
 		DbManager dBManager = ServiceLocator.getDbManager(cDBGWASpi.DB_DATACENTER);
-		List<Map<String, Object>> rs = dBManager.executeSelectStatement("SELECT * FROM " + cDBGWASpi.SCH_MATRICES + "." + cDBMatrix.T_MATRICES + " WHERE " + cDBMatrix.f_ID + "=" + _matrixId + "  WITH RR");
+		String statement = String.format(SQL_STATEMENT_SELECT_MATRIX_BY_ID, matrixId);
+		List<Map<String, Object>> rs = dBManager.executeSelectStatement(statement);
 
-		matrixId = _matrixId;
+		this.matrixId = matrixId;
 
-		//PREVENT PHANTOM-DB READS EXCEPTIONS
+		loadFromResultRest(rs);
+	}
+
+	public MatrixMetadata(String netCDFname) throws IOException {
+
+		DbManager dBManager = ServiceLocator.getDbManager(cDBGWASpi.DB_DATACENTER);
+		String statement = String.format(SQL_STATEMENT_SELECT_MATRIX_BY_NAME, netCDFname);
+		List<Map<String, Object>> rs = dBManager.executeSelectStatement(statement);
+
 		if (!rs.isEmpty() && rs.get(0).size() == cDBMatrix.T_CREATE_MATRICES.length) {
-			matrixFriendlyName = (rs.get(0).get(cDBMatrix.f_MATRIX_NAME) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_NAME).toString() : ""; //matrix_name VARCHAR(64) NOT NULL
-			matrixNetCDFName = (rs.get(0).get(cDBMatrix.f_NETCDF_NAME) != null) ? rs.get(0).get(cDBMatrix.f_NETCDF_NAME).toString() : "";   //netcdf_name VARCHAR(64) NOT NULL
-			matrixType = (rs.get(0).get(cDBMatrix.f_MATRIX_TYPE) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_TYPE).toString() : "";         //matrix_type VARCHAR(32) NOT NULL
-			parentMatrixId1 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID).toString()) : -1;    //parent_matrix1_id INTEGER
-			parentMatrixId2 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID).toString()) : -1;    //parent_matrix2_id INTEGER
-			input_location = (rs.get(0).get(cDBMatrix.f_INPUT_LOCATION) != null) ? rs.get(0).get(cDBMatrix.f_INPUT_LOCATION).toString() : "";     //input_location VARCHAR(1000)
-			description = (rs.get(0).get(cDBMatrix.f_DESCRIPTION) != null) ? rs.get(0).get(cDBMatrix.f_DESCRIPTION).toString() : "";        //description VARCHAR(2000)
-			loaded = (rs.get(0).get(cDBMatrix.f_LOADED) != null) ? rs.get(0).get(cDBMatrix.f_LOADED).toString() : "0";             //loaded CHAR(1)
+			this.matrixId = Integer.parseInt(rs.get(0).get(cDBMatrix.f_ID).toString());
+		}
+
+		loadFromResultRest(rs);
+	}
+
+    /**
+	 * This Method used to import GWASpi matrix from an external file
+	 * The size of this Map is very small.
+	 */
+	public MatrixMetadata(String netCDFpath, int studyId, String newMatrixName) throws IOException {
+
+		this.matrixId = Integer.MIN_VALUE;
+		this.matrixFriendlyName = newMatrixName;
+		this.matrixNetCDFName = org.gwaspi.netCDF.matrices.MatrixManager.generateMatrixNetCDFNameByDate();
+		this.studyId = studyId;
+
+		this.pathToMatrix = netCDFpath;
+		loadMatrixFromFile();
+	}
+
+	private void loadFromResultRest(List<Map<String, Object>> rs) throws IOException {
+
+		// PREVENT PHANTOM-DB READS EXCEPTIONS
+		if (!rs.isEmpty() && rs.get(0).size() == cDBMatrix.T_CREATE_MATRICES.length) {
+			matrixId = Integer.parseInt(rs.get(0).get(cDBMatrix.f_ID).toString());
+			matrixFriendlyName = (rs.get(0).get(cDBMatrix.f_MATRIX_NAME) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_NAME).toString() : ""; // matrix_name VARCHAR(64) NOT NULL
+			matrixNetCDFName = (rs.get(0).get(cDBMatrix.f_NETCDF_NAME) != null) ? rs.get(0).get(cDBMatrix.f_NETCDF_NAME).toString() : ""; // netcdf_name VARCHAR(64) NOT NULL
+			matrixType = (rs.get(0).get(cDBMatrix.f_MATRIX_TYPE) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_TYPE).toString() : ""; // matrix_type VARCHAR(32) NOT NULL
+			parentMatrixId1 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID).toString()) : -1; // parent_matrix1_id INTEGER
+			parentMatrixId2 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID).toString()) : -1; // parent_matrix2_id INTEGER
+			input_location = (rs.get(0).get(cDBMatrix.f_INPUT_LOCATION) != null) ? rs.get(0).get(cDBMatrix.f_INPUT_LOCATION).toString() : ""; // input_location VARCHAR(1000)
+			description = (rs.get(0).get(cDBMatrix.f_DESCRIPTION) != null) ? rs.get(0).get(cDBMatrix.f_DESCRIPTION).toString() : ""; // description VARCHAR(2000)
+			loaded = (rs.get(0).get(cDBMatrix.f_LOADED) != null) ? rs.get(0).get(cDBMatrix.f_LOADED).toString() : "0"; // loaded CHAR(1)
 			studyId = (rs.get(0).get(cDBMatrix.f_STUDYID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_STUDYID).toString()) : 0;
 		}
 
+		String genotypesFolder = Config.getConfigValue(Config.PROPERTY_GENOTYPES_DIR, "");
+		String pathToStudy = genotypesFolder + "/STUDY_" + studyId + "/";
+		pathToMatrix = pathToStudy + matrixNetCDFName + ".nc";
+		loadMatrixFromFile();
+	}
+
+	private void loadMatrixFromFile() throws IOException {
 
 		String genotypesFolder = Config.getConfigValue(Config.PROPERTY_GENOTYPES_DIR, "");
 		String pathToStudy = genotypesFolder + "/STUDY_" + studyId + "/";
@@ -91,128 +138,6 @@ public class MatrixMetadata {
 						ArrayChar.D2 gtCodeAC = (ArrayChar.D2) var.read("(0:0:1, 0:7:1)");
 //						gtEncoding = GenotypeEncoding.valueOf(gtCodeAC.getString(0));
 						gtEncoding = GenotypeEncoding.compareTo(gtCodeAC.getString(0)); // HACK, the above was used before
-					} catch (InvalidRangeException ex) {
-						log.error(null, ex);
-					}
-				}
-
-				strand = StrandType.valueOf(ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_STRAND).getStringValue());
-				hasDictionray = ((Integer) ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_HAS_DICTIONARY).getNumericValue() != 0);
-
-				Dimension markerSetDim = ncfile.findDimension(cNetCDF.Dimensions.DIM_MARKERSET);
-				markerSetSize = markerSetDim.getLength();
-
-				Dimension sampleSetDim = ncfile.findDimension(cNetCDF.Dimensions.DIM_SAMPLESET);
-				sampleSetSize = sampleSetDim.getLength();
-			} catch (IOException ex) {
-				log.error("Cannot open file: " + ncfile, ex);
-			} finally {
-				if (null != ncfile) {
-					try {
-						ncfile.close();
-					} catch (IOException ex) {
-						log.warn("Cannot close file: " + ncfile, ex);
-					}
-				}
-			}
-		}
-	}
-
-	public MatrixMetadata(String netCDFname) throws IOException {
-		DbManager dBManager = ServiceLocator.getDbManager(cDBGWASpi.DB_DATACENTER);
-		List<Map<String, Object>> rs = dBManager.executeSelectStatement("SELECT * FROM " + cDBGWASpi.SCH_MATRICES + "." + cDBMatrix.T_MATRICES + " WHERE " + cDBMatrix.f_NETCDF_NAME + "='" + netCDFname + "'  WITH RR");
-//        rs = dBManager.executeSelectStatement("SELECT * FROM "+cDBGWASpi.SCH_MATRICES+"."+cDBMatrix.T_MATRICES+" WHERE "+cDBMatrix.f_INPUT_LOCATION+"='"+netCDFname+"'  WITH RR");
-
-		//PREVENT PHANTOM-DB READS EXCEPTIONS
-		if (!rs.isEmpty() && rs.get(0).size() == cDBMatrix.T_CREATE_MATRICES.length) {
-			matrixId = Integer.parseInt(rs.get(0).get(cDBMatrix.f_ID).toString());
-			matrixFriendlyName = (rs.get(0).get(cDBMatrix.f_MATRIX_NAME) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_NAME).toString() : ""; //matrix_name VARCHAR(64) NOT NULL
-			matrixNetCDFName = (rs.get(0).get(cDBMatrix.f_NETCDF_NAME) != null) ? rs.get(0).get(cDBMatrix.f_NETCDF_NAME).toString() : "";   //netcdf_name VARCHAR(64) NOT NULL
-			matrixType = (rs.get(0).get(cDBMatrix.f_MATRIX_TYPE) != null) ? rs.get(0).get(cDBMatrix.f_MATRIX_TYPE).toString() : "";         //matrix_type VARCHAR(32) NOT NULL
-			parentMatrixId1 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX1_ID).toString()) : -1;    //parent_matrix1_id INTEGER
-			parentMatrixId2 = (rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_PARENT_MATRIX2_ID).toString()) : -1;    //parent_matrix2_id INTEGER
-			input_location = (rs.get(0).get(cDBMatrix.f_INPUT_LOCATION) != null) ? rs.get(0).get(cDBMatrix.f_INPUT_LOCATION).toString() : "";     //input_location VARCHAR(1000)
-			description = (rs.get(0).get(cDBMatrix.f_DESCRIPTION) != null) ? rs.get(0).get(cDBMatrix.f_DESCRIPTION).toString() : "";        //description VARCHAR(2000)
-			loaded = (rs.get(0).get(cDBMatrix.f_LOADED) != null) ? rs.get(0).get(cDBMatrix.f_LOADED).toString() : "0";             //loaded CHAR(1)
-			studyId = (rs.get(0).get(cDBMatrix.f_STUDYID) != null) ? Integer.parseInt(rs.get(0).get(cDBMatrix.f_STUDYID).toString()) : 0;
-
-		}
-
-		String genotypesFolder = Config.getConfigValue(Config.PROPERTY_GENOTYPES_DIR, "");
-		String pathToStudy = genotypesFolder + "/STUDY_" + studyId + "/";
-		pathToMatrix = pathToStudy + matrixNetCDFName + ".nc";
-		NetcdfFile ncfile = null;
-		if (new File(pathToMatrix).exists()) {
-			try {
-				ncfile = NetcdfFile.open(pathToMatrix);
-
-				technology = ImportFormat.compareTo(ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_TECHNOLOGY).getStringValue());
-				try {
-					gwaspiDBVersion = ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_GWASPIDB_VERSION).getStringValue();
-				} catch (Exception ex) {
-					log.error(null, ex);
-				}
-
-				Variable var = ncfile.findVariable(cNetCDF.Variables.GLOB_GTENCODING);
-				if (var != null) {
-					try {
-						ArrayChar.D2 gtCodeAC = (ArrayChar.D2) var.read("(0:0:1, 0:7:1)");
-						gtEncoding = GenotypeEncoding.valueOf(gtCodeAC.getString(0));
-					} catch (InvalidRangeException ex) {
-						log.error(null, ex);
-					}
-				}
-
-				strand = StrandType.valueOf(ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_STRAND).getStringValue());
-				hasDictionray = ((Integer) ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_HAS_DICTIONARY).getNumericValue() != 0);
-
-				Dimension markerSetDim = ncfile.findDimension(cNetCDF.Dimensions.DIM_MARKERSET);
-				markerSetSize = markerSetDim.getLength();
-
-				Dimension sampleSetDim = ncfile.findDimension(cNetCDF.Dimensions.DIM_SAMPLESET);
-				sampleSetSize = sampleSetDim.getLength();
-			} catch (IOException ex) {
-				log.error("Cannot open file: " + ncfile, ex);
-			} finally {
-				if (null != ncfile) {
-					try {
-						ncfile.close();
-					} catch (IOException ex) {
-						log.warn("Cannot close file: " + ncfile, ex);
-					}
-				}
-			}
-		}
-	}
-
-    /**
-	 * This Method used to import GWASpi matrix from an external file
-	 * The size of this Map is very small.
-	 */
-	public MatrixMetadata(String netCDFpath, int _studyId, String newMatrixName) throws IOException {
-		matrixId = Integer.MIN_VALUE;
-		matrixFriendlyName = newMatrixName;
-		matrixNetCDFName = org.gwaspi.netCDF.matrices.MatrixManager.generateMatrixNetCDFNameByDate();
-		studyId = _studyId;
-
-		pathToMatrix = netCDFpath;
-		NetcdfFile ncfile = null;
-		if (new File(pathToMatrix).exists()) {
-			try {
-				ncfile = NetcdfFile.open(pathToMatrix);
-
-				technology = ImportFormat.compareTo(ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_TECHNOLOGY).getStringValue());
-				try {
-					gwaspiDBVersion = ncfile.findGlobalAttribute(cNetCDF.Attributes.GLOB_GWASPIDB_VERSION).getStringValue();
-				} catch (Exception ex) {
-					log.error(null, ex);
-				}
-
-				Variable var = ncfile.findVariable(cNetCDF.Variables.GLOB_GTENCODING);
-				if (var != null) {
-					try {
-						ArrayChar.D2 gtCodeAC = (ArrayChar.D2) var.read("(0:0:1, 0:7:1)");
-						gtEncoding = GenotypeEncoding.valueOf(gtCodeAC.getString(0));
 					} catch (InvalidRangeException ex) {
 						log.error(null, ex);
 					}
