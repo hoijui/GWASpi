6828c633d2c2f2c44a0a13bc75c6c3f22d6fd788
hoijui <hoijui.quaero@gmail.com>
2013-05-17 14:44:44 +0200

    remove an unused import [nlc]

--- src/main/java/org/gwaspi/model/Operation.java
+++ src/main/java/org/gwaspi/model/Operation.java
@@ -3,7 +3,6 @@ package org.gwaspi.model;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
