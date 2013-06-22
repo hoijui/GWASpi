9859ee41ed4abefe20bbd59969f2ff87f9140b27
hoijui <hoijui.quaero@gmail.com>
2013-06-22 10:39:56 +0200

    a minor simplification of code in OP_MarkerCensus

--- src/main/java/org/gwaspi/netCDF/operations/OP_MarkerCensus.java
+++ src/main/java/org/gwaspi/netCDF/operations/OP_MarkerCensus.java
@@ -366,7 +366,6 @@ public class OP_MarkerCensus implements MatrixOperation {
 
 						wrChunkedMarkerCensusMap.put(markerKey, census);
 
-						StringBuilder sb = new StringBuilder();
 						byte[] alleles = cNetCDF.Defaults.DEFAULT_GT;
 						Iterator<Byte> knit = knownAlleles.keySet().iterator();
 						if (knownAlleles.size() == 2) {
@@ -378,9 +377,8 @@ public class OP_MarkerCensus implements MatrixOperation {
 							Byte allele1 = knit.next();
 							alleles = new byte[] {allele1, allele1};
 						}
-						sb.append(new String(alleles));
 
-						wrChunkedKnownAllelesMap.put(markerKey, sb.toString().toCharArray());
+						wrChunkedKnownAllelesMap.put(markerKey, new String(alleles).toCharArray());
 					} else {
 						// MISMATCHES FOUND
 						wrChunkedMarkerCensusMap.put(markerKey, new Census());
