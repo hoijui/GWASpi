73b7ca6d20474efee4c6e27197b050212115a6ba
hoijui <hoijui.quaero@gmail.com>
2014-12-03 11:38:12 +0100

    IntroPanel: exchange GroupLayout with GridBagLayout

--- src/main/java/org/gwaspi/gui/IntroPanel.java
+++ src/main/java/org/gwaspi/gui/IntroPanel.java
@@ -18,10 +18,11 @@
 package org.gwaspi.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -30,7 +31,6 @@ import java.net.URL;
 import javax.swing.AbstractAction;
 import javax.swing.AbstractListModel;
 import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -38,8 +38,6 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
-import javax.swing.LayoutStyle;
-import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 import org.gwaspi.global.Text;
 import org.gwaspi.gui.utils.HelpURLs;
@@ -53,26 +51,52 @@ public class IntroPanel extends JPanel {
 
 	public IntroPanel() {
 
+		final GridBagLayout layout = new GridBagLayout();
+		final int gapSpace = 5;
+		// we use every second row and column as a spacer
+		layout.columnWidths = new int[] {0, gapSpace, 0, gapSpace, 0};
+		layout.rowHeights = new int[] {0, gapSpace, 0, gapSpace, 0, gapSpace, 0};
+		setLayout(layout);
+
+		final GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.gridheight = 1;
+		gridBagConstraints.fill = GridBagConstraints.BOTH;
+		gridBagConstraints.weightx = 0.0;
+		gridBagConstraints.weighty = 0.0;
+
 		//setBorder(BorderFactory.createTitledBorder(null, Text.App.appName, border.TitledBorder.DEFAULT_JUSTIFICATION, border.TitledBorder.DEFAULT_POSITION, new Font("FreeSans", 0, 24))); // NOI18N
 		setBorder(BorderFactory.createTitledBorder(""));
 
-		//<editor-fold defaultstate="expanded" desc="LOGOS LAYOUT">
 		final JButton btn_logo = new JButton();
 		btn_logo.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btn_logo.setAction(new BrowseHomepageAction());
 		btn_logo.setMaximumSize(new Dimension(185, 150));
 		btn_logo.setPreferredSize(new Dimension(185, 150));
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		add(btn_logo, gridBagConstraints);
 
 		final JButton btn_logoIbe = new JButton();
 		btn_logoIbe.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btn_logoIbe.setAction(new BrowseInbAction());
-		//</editor-fold>
+		gridBagConstraints.gridx = 4;
+		gridBagConstraints.gridy = 4;
+		add(btn_logoIbe, gridBagConstraints);
 
 		final JScrollPane scroll_About = new JScrollPane();
 		final JTextPane txtP_About = new JTextPane();
 		txtP_About.setEditable(false);
 		txtP_About.setText(Text.App.appDescription);
+		// we need this, because JTextPane deals wrongly with the line breaks in the text
+		// resulting in a way too big preffered width
+		txtP_About.setPreferredSize(new Dimension(200, 150));
 		scroll_About.setViewportView(txtP_About);
+		gridBagConstraints.gridx = 2;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 3;
+		gridBagConstraints.weightx = 1.0;
+		add(scroll_About, gridBagConstraints);
 
 		final JScrollPane scroll_Help = new JScrollPane();
 		final JList list_Help = new JList();
@@ -108,8 +132,13 @@ public class IntroPanel extends JPanel {
 			}
 		});
 		scroll_Help.setViewportView(list_Help);
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridwidth = 5;
+		gridBagConstraints.weightx = 1.0;
+		gridBagConstraints.weighty = 1.0;
+		add(scroll_Help, gridBagConstraints);
 
-		//<editor-fold defaultstate="expanded" desc="CONTACT">
 		final JScrollPane scroll_Contact = new JScrollPane();
 		final JTextPane txtP_Contact = new JTextPane();
 		txtP_Contact.setText(
@@ -118,62 +147,40 @@ public class IntroPanel extends JPanel {
 				+ "Authors: " + Text.App.authors + "\n"
 				+ "License: " + Text.App.license);
 		scroll_Contact.setViewportView(txtP_Contact);
-		//</editor-fold>
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 4;
+		gridBagConstraints.gridwidth = 3;
+		gridBagConstraints.weightx = 1.0;
+		gridBagConstraints.weighty = 0.0;
+		add(scroll_Contact, gridBagConstraints);
+		// limit the preffered width a bit
+		txtP_Contact.setPreferredSize(new Dimension(400, 100));
+
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.weightx = 0.0;
+		gridBagConstraints.fill = GridBagConstraints.NONE;
+
+		final JButton btn_preferences = new JButton();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 6;
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		add(btn_preferences, gridBagConstraints);
 
 		final JButton btn_exit = new JButton();
-		final JButton btn_preferences = new JButton();
+		gridBagConstraints.gridx = 2;
+		gridBagConstraints.gridy = 6;
+		gridBagConstraints.anchor = GridBagConstraints.EAST;
+		add(btn_exit, gridBagConstraints);
+
 		final JButton btn_start = new JButton();
+		gridBagConstraints.gridx = 4;
+		gridBagConstraints.gridy = 6;
+		gridBagConstraints.anchor = GridBagConstraints.EAST;
+		add(btn_start, gridBagConstraints);
 
 		btn_exit.setAction(new ExitAction());
 		btn_preferences.setAction(new OpenPreferencesAction());
 		btn_start.setAction(new StartAction());
-
-		// <editor-fold defaultstate="expanded" desc="LAYOUT">
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		layout.setHorizontalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-				.addComponent(scroll_Help, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 660, Short.MAX_VALUE)
-				.addGroup(GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-				.addComponent(btn_logo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addComponent(scroll_About, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE))
-				.addGroup(GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-				.addComponent(scroll_Contact, GroupLayout.DEFAULT_SIZE, 398, Short.MAX_VALUE)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addComponent(btn_logoIbe, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)))
-				.addContainerGap())
-				.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-				.addGap(30, 30, 30)
-				.addComponent(btn_preferences)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 433, Short.MAX_VALUE)
-				.addComponent(btn_exit)
-				.addGap(30, 30, 30)
-				.addComponent(btn_start)
-				.addGap(29, 29, 29)));
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-				.addComponent(scroll_About, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE)
-				.addComponent(btn_logo, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE))
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addComponent(scroll_Help, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
-				.addGap(11, 11, 11)
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addComponent(scroll_Contact, GroupLayout.DEFAULT_SIZE, 122, GroupLayout.PREFERRED_SIZE)
-				.addComponent(btn_logoIbe, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addGap(18, 18, 18)
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(btn_start)
-				.addComponent(btn_exit)
-				.addComponent(btn_preferences))
-				.addContainerGap()));
-		layout.linkSize(SwingConstants.VERTICAL, new Component[] {scroll_Contact, btn_logoIbe});
-		// </editor-fold>
 	}
 
 	private static class OpenPreferencesAction extends AbstractAction {
