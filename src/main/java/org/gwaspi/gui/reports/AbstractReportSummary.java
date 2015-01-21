eff54df84ca40917d29d9462cb54b7b976ee8367
hoijui <hoijui.quaero@gmail.com>
2015-01-21 10:45:42 +0100

    merge three Report_* classes into AbstractReportSummary

---
+++ src/main/java/org/gwaspi/gui/reports/AbstractReportSummary.java
@@ -0,0 +1,230 @@
+/*
+ * Copyright (C) 2013 Universitat Pompeu Fabra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gwaspi.gui.reports;
+
+import java.awt.Component;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.Action;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+import org.gwaspi.global.Text;
+import org.gwaspi.gui.BackAction;
+import org.gwaspi.gui.GWASpiExplorerPanel;
+import org.gwaspi.gui.utils.BrowserHelpUrlAction;
+import org.gwaspi.gui.utils.IntegerInputVerifier;
+import org.gwaspi.gui.utils.RowRendererDefault;
+import org.gwaspi.model.DataSetKey;
+import org.gwaspi.model.OperationKey;
+import org.gwaspi.model.Study;
+import org.gwaspi.reports.ReportParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractReportSummary extends JPanel {
+
+	private static final Logger log
+			= LoggerFactory.getLogger(AbstractReportSummary.class);
+
+	protected AbstractReportSummary(
+			final OperationKey operationKey,
+			final String reportFileName,
+			final String reportName,
+			final String nRowsSuffix,
+			final String helpUrlSuffix,
+			final ReportParser reportParser)
+	{
+		String reportPath = "";
+		try {
+			reportPath = Study.constructReportsPath(operationKey.getParentMatrixKey().getStudyKey());
+		} catch (IOException ex) {
+			log.error(null, ex);
+		}
+		final File reportFile = new File(reportPath + reportFileName);
+
+		final JButton btn_Get;
+		final JButton btn_Save;
+		final JButton btn_Back;
+		final JButton btn_Help;
+		final JPanel pnl_Footer;
+		final JPanel pnl_Summary;
+		final JLabel lbl_suffix1;
+		final JScrollPane scrl_ReportTable;
+		final JTable tbl_ReportTable;
+		final JFormattedTextField txt_NRows;
+
+		pnl_Summary = new JPanel();
+		txt_NRows = new JFormattedTextField();
+		txt_NRows.setInputVerifier(new IntegerInputVerifier());
+		txt_NRows.setValue(100);
+		txt_NRows.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent evt) {
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						txt_NRows.selectAll();
+					}
+				});
+			}
+		});
+		lbl_suffix1 = new JLabel();
+		btn_Get = new JButton();
+		scrl_ReportTable = new JScrollPane();
+		tbl_ReportTable = new JTable() {
+			@Override
+			public boolean isCellEditable(int row, int col) {
+				return false;
+			}
+		};
+		tbl_ReportTable.setDefaultRenderer(Object.class, new RowRendererDefault());
+
+		pnl_Footer = new JPanel();
+		btn_Save = new JButton();
+		btn_Back = new JButton();
+		btn_Help = new JButton();
+
+		setBorder(GWASpiExplorerPanel.createMainTitledBorder(
+				Text.Reports.report + ": " + reportName)); // NOI18N
+
+		pnl_Summary.setBorder(GWASpiExplorerPanel.createRegularTitledBorder(Text.Reports.summary));
+
+		final Action loadReportAction = new LoadReportAction(
+				reportFile, tbl_ReportTable, txt_NRows, reportParser);
+
+		txt_NRows.setHorizontalAlignment(JFormattedTextField.TRAILING);
+		lbl_suffix1.setText(nRowsSuffix);
+
+		//<editor-fold defaultstate="expanded" desc="LAYOUT1">
+		GroupLayout pnl_SummaryLayout = new GroupLayout(pnl_Summary);
+		pnl_Summary.setLayout(pnl_SummaryLayout);
+		pnl_SummaryLayout.setHorizontalGroup(
+				pnl_SummaryLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(pnl_SummaryLayout.createSequentialGroup()
+				.addContainerGap()
+				.addComponent(txt_NRows, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(lbl_suffix1)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 152, Short.MAX_VALUE)
+				.addComponent(btn_Get, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap()));
+		pnl_SummaryLayout.setVerticalGroup(
+				pnl_SummaryLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(pnl_SummaryLayout.createSequentialGroup()
+				.addGroup(pnl_SummaryLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				.addComponent(txt_NRows, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addComponent(lbl_suffix1)
+				.addComponent(btn_Get))
+				.addContainerGap()));
+		//</editor-fold>
+
+		scrl_ReportTable.setViewportView(tbl_ReportTable);
+
+		//<editor-fold defaultstate="expanded" desc="FOOTER">
+		GroupLayout pnl_FooterLayout = new GroupLayout(pnl_Footer);
+		pnl_Footer.setLayout(pnl_FooterLayout);
+		pnl_FooterLayout.setHorizontalGroup(
+				pnl_FooterLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(GroupLayout.Alignment.TRAILING, pnl_FooterLayout.createSequentialGroup()
+				.addContainerGap()
+				.addComponent(btn_Back, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
+				.addGap(18, 18, 18)
+				.addComponent(btn_Help, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 321, Short.MAX_VALUE)
+				.addComponent(btn_Save, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap()));
+		pnl_FooterLayout.linkSize(SwingConstants.HORIZONTAL, new Component[]{btn_Back, btn_Help});
+		pnl_FooterLayout.setVerticalGroup(
+				pnl_FooterLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(pnl_FooterLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				.addComponent(btn_Save)
+				.addComponent(btn_Back)
+				.addComponent(btn_Help)));
+		//</editor-fold>
+
+		//<editor-fold defaultstate="expanded" desc="LAYOUT">
+		GroupLayout layout = new GroupLayout(this);
+		this.setLayout(layout);
+		layout.setHorizontalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addComponent(pnl_Footer, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+				.addComponent(scrl_ReportTable, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 672, Short.MAX_VALUE)
+				.addComponent(pnl_Summary, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+				.addContainerGap()));
+		layout.setVerticalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+				.addComponent(pnl_Summary, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(scrl_ReportTable)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(pnl_Footer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap()));
+		//</editor-fold>
+
+		tbl_ReportTable.setModel(new DefaultTableModel(
+				new Object[][] {
+					{null, null, null, "Go!"}
+				},
+				new String[] {"", "", "", ""}));
+
+		txt_NRows.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				final int key = e.getKeyChar();
+				if (key == KeyEvent.VK_ENTER) {
+					loadReportAction.actionPerformed(null);
+				}
+			}
+		});
+		btn_Get.setAction(loadReportAction);
+		btn_Save.setAction(new Report_Analysis.SaveAsAction(
+				operationKey.getParentMatrixKey().getStudyKey(),
+				reportFileName,
+				tbl_ReportTable,
+				txt_NRows));
+		btn_Back.setAction(new BackAction(new DataSetKey(operationKey)));
+		btn_Help.setAction(new BrowserHelpUrlAction(helpUrlSuffix));
+
+		loadReportAction.actionPerformed(null);
+	}
+
+	protected static String extractPartAfterFirstDash(final String originalStr) {
+		return originalStr.substring(originalStr.indexOf('-') + 2);
+	}
+
+	protected static String getReportName(/*final OperationKey operation*/) {
+		return extractPartAfterFirstDash(GWASpiExplorerPanel.getSingleton().getTree().getLastSelectedPathComponent().toString()); // HACK XXX better fetch through operation meta-data?
+	}
+}
